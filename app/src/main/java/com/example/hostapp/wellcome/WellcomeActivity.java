07e19d04d4ef52236e290510dd465f384806ea80
I553132 <I553132@W-PF1MYMK5>
2022-05-16 01:43:39 +0500

    add splash screen

--- app/src/main/java/com/example/hostapp/wellcome/WellcomeActivity.java
+++ app/src/main/java/com/example/hostapp/wellcome/WellcomeActivity.java
@@ -1,15 +1,21 @@
 package com.example.hostapp.wellcome;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.hostapp.R;
+import com.example.hostapp.login.LoginActivity;
 
 public class WellcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wellcome_page);}
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
