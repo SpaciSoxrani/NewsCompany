da5a94c102ca2227f4b9e6a2fa8a1c7bae03fdce
I553132 <I553132@W-PF1MYMK5>
2022-05-22 18:39:38 +0500

    delete legacy code and warnings

--- app/src/main/java/com/example/hostapp/mainMenu/MainActivity.java
+++ app/src/main/java/com/example/hostapp/mainMenu/MainActivity.java
@@ -1,92 +1,67 @@
 package com.example.hostapp.mainMenu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentContainerView;
-import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.util.Log;
-
 import com.example.hostapp.R;
 import com.example.hostapp.mails.MailingItem;
-import com.example.hostapp.preSale.NewMailing;
 import com.example.hostapp.serverapi.APINews.RiaNewsModel;
-import com.example.hostapp.serverapi.APIPreSaleDetailsModels.RegionsModel;
-import com.example.hostapp.serverapi.APIPreSaleDetailsModels.StatusesModel;
-import com.example.hostapp.serverapi.APIPreSaleModels.MainDepartmentsModel;
-import com.example.hostapp.serverapi.APIPreSaleModels.PreSaleGroupStatusesModel;
 import com.example.hostapp.serverapi.App;
 import com.example.hostapp.serverapi.DemoServerApi;
-import com.example.hostapp.serverapi.APIPreSaleModels.PreSaleEntryModel;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity{
 
-    private MaterialToolbar topToolbar;
-    private NavController navController;
-
-    FragmentTransaction transaction;
-    Fragment mainMenuFragment;
-    private FragmentManager manager;
-
-    //List<PreSaleEntryModel> preSaleList;
-    //List<MainDepartmentsModel> mainDepartmentsModelList;
     List<RiaNewsModel> riaNewsList;
 
     private static final String TAG = "PreSaleFragment";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        topToolbar = findViewById(R.id.topAppBar);
-        //topToolbar.setTitle(R.string.title_main);
+        MaterialToolbar topToolbar = findViewById(R.id.topAppBar);
 
         topToolbar.setNavigationIcon(R.drawable.ic_menu_top_toolbar_24);
-        manager = getSupportFragmentManager();
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_mails, R.id.navigation_filters_mail)
                 .build();
         final FragmentContainerView containerView = findViewById(R.id.nav_host_fragment);
-        navController = Navigation.findNavController(containerView);
+        NavController navController = Navigation.findNavController(containerView);
         NavigationUI.setupWithNavController(navView, navController);
 
         riaNewsList = new ArrayList<>();
-        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        //preSaleList = new ArrayList<>();
-        //mainDepartmentsModelList = new ArrayList<>();
-
         App.getNewsApi().getRiaNews().enqueue(new Callback<List<RiaNewsModel>>() {
             @Override
-            public void onResponse(Call<List<RiaNewsModel>> call, Response<List<RiaNewsModel>> response) {
-                if(response.body() == null){Log.i(TAG, "response.body == null");}
-                else {
+            public void onResponse(@NonNull Call<List<RiaNewsModel>> call, @NonNull Response<List<RiaNewsModel>> response) {
+                if (response.body() == null) {
+                    Log.i(TAG, "response.body == null");
+                } else {
                     DemoServerApi.NEW_MAILINGS.clear();
                     riaNewsList.addAll(response.body());
                     for (int i = 0; i < riaNewsList.size(); i++) {
                         MailingItem newMailing = new MailingItem(
                                 R.drawable.menu_item_orange,
-                                riaNewsList.get(i).getName().toString(),
-                                riaNewsList.get(i).getPrediction().toString(),
+                                riaNewsList.get(i).getName(),
+                                riaNewsList.get(i).getPrediction(),
                                 riaNewsList.get(i).getProbability(),
-                                riaNewsList.get(i).getDateTime().toString(),
-                                riaNewsList.get(i).getId().toString());
+                                riaNewsList.get(i).getDateTime(),
+                                riaNewsList.get(i).getId());
 
                         DemoServerApi.TEST_MAILINGS.add(newMailing);
                     }
@@ -95,126 +70,9 @@ public class MainActivity extends AppCompatActivity{
             }
 
             @Override
-            public void onFailure(Call<List<RiaNewsModel>> call, Throwable t) {
+            public void onFailure(@NonNull Call<List<RiaNewsModel>> call, @NonNull Throwable t) {
                 Log.i(TAG, "An error occurred during networking!");
             }
         });
-
-
-//        App.getPreSalesApi().getData().enqueue(new Callback<List<PreSaleEntryModel>>() {
-//            @Override
-//            public void onResponse(Call<List<PreSaleEntryModel>> call, Response<List<PreSaleEntryModel>> response) {
-//                if(response.body() == null){Log.i(TAG, "response.body == null");}
-//                else {
-//                    DemoServerApi.NEW_MAILINGS.clear();
-//                    preSaleList.addAll(response.body());
-//                    for (int i = 0; i < preSaleList.size(); i++) {
-//                        NewMailing newMailing = new NewMailing(3, preSaleList.get(i).getName().toString(),
-//                                preSaleList.get(i).getStatus().toString(), preSaleList.get(i).getDepartment().toString(), null, preSaleList.get(i).getId());
-//
-//                        DemoServerApi.NEW_MAILINGS.add(newMailing);
-//                    }
-//                    Log.i(TAG, "OK OK OK");
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<List<PreSaleEntryModel>> call, Throwable t) {
-//                Log.i(TAG, "An error occurred during networking!");
-//            }
-//        });
-//
-//        App.getPreSalesApi().getMainDepartments().enqueue(new Callback<List<MainDepartmentsModel>>() {
-//            @Override
-//            public void onResponse(Call<List<MainDepartmentsModel>> call, Response<List<MainDepartmentsModel>> response) {
-//                if(response.body() == null){Log.i(TAG, "response.body == null");}
-//                else {
-//                    DemoServerApi.MAIN_DEPARTMENTS_MODEL_LIST.clear();
-//                    DemoServerApi.MAIN_DEPARTMENTS_MODEL_LIST.addAll(response.body());
-//                    Log.i(TAG, "Get main departments list");
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<List<MainDepartmentsModel>> call, Throwable t) {
-//                Log.i(TAG, "Enable to get main departments!");
-//            }
-//        });
-//
-//        App.getPreSalesApi().getPresaleGroupStatuses().enqueue(new Callback<List<PreSaleGroupStatusesModel>>() {
-//            @Override
-//            public void onResponse(Call<List<PreSaleGroupStatusesModel>> call, Response<List<PreSaleGroupStatusesModel>> response) {
-//                if(response.body() == null){Log.i(TAG, "response.body == null");}
-//                else {
-//                    DemoServerApi.PRE_SALE_GROUP_STATUSES_MODEL_LIST.clear();
-//                    DemoServerApi.PRE_SALE_GROUP_STATUSES_MODEL_LIST.addAll(response.body());
-//                    Log.i(TAG, "Get group statuses");
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<List<PreSaleGroupStatusesModel>> call, Throwable t) {
-//                Log.i(TAG, "Enable to get group statuses!");
-//            }
-//        });
-//
-//        App.getDetailsApi().getRegions().enqueue(new Callback<List<RegionsModel>>() {
-//            @Override
-//            public void onResponse(Call<List<RegionsModel>> call, Response<List<RegionsModel>> response) {
-//                if(response.body() == null){
-//                    Log.i(TAG, "response.body of regions == null");}
-//                else {
-//                    DemoServerApi.REGIONS_MODEL_LIST.clear();
-//                    DemoServerApi.REGIONS_MODEL_LIST.addAll(response.body());
-//                    Log.i(TAG, "Get list regions");
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<List<RegionsModel>> call, Throwable t) {
-//                Log.i(TAG, "Enable to get list regions!");
-//            }
-//        });
-//
-//        App.getDetailsApi().getStatuses().enqueue(new Callback<List<StatusesModel>>() {
-//            @Override
-//            public void onResponse(Call<List<StatusesModel>> call, Response<List<StatusesModel>> response) {
-//                if(response.body() == null){
-//                    Log.i(TAG, "response.body of statuses == null");}
-//                else {
-//                    DemoServerApi.STATUSES_MODEL_LIST.clear();
-//                    DemoServerApi.STATUSES_MODEL_LIST.addAll(response.body());
-//                    Log.i(TAG, "Get list statuses");
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<List<StatusesModel>> call, Throwable t) {
-//                Log.i(TAG, "Enable to get list statuses!");
-//            }
-//        });
-
-//        final FragmentContainerView containerView = findViewById(R.id.nav_host_fragment);
-//        navController = Navigation.findNavController(containerView);
-
-
-        //todo create navigation class !!!
-//        topToolbar.setOnMenuItemClickListener(item -> {
-//            if (item.getItemId() == R.id.nav_profile_icon) {
-//                this.navController.navigate(R.id.action_navigation_main_menu_to_navigation_profile);
-//
-//            } else {
-//            }
-//            return false;
-//        });
-
     }
-
-
-//    @Override
-//    public void onBackPressed() {
-//        // super.onBackPressed();
-//        Fragment mainMenu = new MainMenuFragment();
-//        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, mainMenu).commit();
-//    }
 }
